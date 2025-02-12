97cf165891230fd2c8e9cee3affdd4f15ef2e524
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-02-12 18:31:03 +0300

    feat: enhance WalletComponent with transfer functionality and state management

--- client_nuno_token/src/components/WalletComponent.jsx
+++ client_nuno_token/src/components/WalletComponent.jsx
@@ -16,8 +16,10 @@ const WalletComponent = () => {
   const [totalSupply, setTotalSupply] = useState("0");
   const [balance, setBalance] = useState("");
   const [error, setError] = useState("");
+  const [amount, setAmount] = useState("");
+  const [receiver, setReceiver] = useState("");
 
-  console.log("Wallet Add", walletAddress);
+  console.log("Wallet Add", String(walletAddress));
 
   // console.log("balance", balance);
 
@@ -32,39 +34,38 @@ const WalletComponent = () => {
             await connectWallet();
           }
         } catch (error) {
-          setError(error)
+          setError(error);
         }
       }
-    }
+    };
     checkConnection();
-  }, [])
-
-
+  }, []);
 
   const connectWallet = async () => {
     try {
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
+      console.log("account", accounts);
 
       const ethersProvider = new ethers.providers.Web3Provider(window.ethereum);
       console.log("ether Pr", ethersProvider);
-      
+
       const ethersSigner = ethersProvider.getSigner();
-      console.log("ether Signer", ethersSigner)
+      console.log("ether Signer", ethersSigner);
       const tokenContract = new ethers.Contract(
         contract_address,
         abi,
         ethersSigner
       );
-      // console.log("Contract", await tokenContract.getBalance(walletAddress));
+      console.log("Contract", await tokenContract.getBalance(accounts[0]));
 
       setProvider(ethersProvider);
       setSigner(ethersSigner);
       setContract(tokenContract);
       setWalletAddress(accounts[0]);
       setIsConnected(true);
-      
+
       // Fetch the details of the token
       const name = await tokenContract.getName();
       console.log("Name", name);
@@ -72,18 +73,42 @@ const WalletComponent = () => {
       console.log("Symbol", symbol);
       const supply = await tokenContract.getTotalSupply();
       console.log("supply", supply);
-      // const userBalance = await tokenContract.getBalance();
-      // console.log("user Balance", userBalance);
-      
+      const userBalance = await tokenContract.getBalance(accounts[0]);
+      console.log("user Balance", userBalance);
+
       setTokenName(name);
       setTokenSymbol(symbol);
       setTotalSupply(ethers.utils.formatUnits(supply, 18));
-      console.log("Supply in number", ethers.utils.formatUnits(supply, 18))
-      // setBalance(ethers.utils.formatUnits(userBalance, 18));
-      // console.log(ethers.utils.formatUnits(userBalance, 18))
-      
+      console.log("Supply in number", ethers.utils.formatUnits(supply, 18));
+      setBalance(ethers.utils.formatUnits(userBalance, 18));
+      console.log(ethers.utils.formatUnits(userBalance, 18));
     } catch (error) {
       setError(error);
+    }
+  };
+
+  const disconnect = () => {
+    setProvider(null);
+    setSigner(null);
+    setContract(null);
+    setWalletAddress("");
+    setIsConnected(false);
+  };
+
+  const handleTransfer = async () => {
+    try {
+      if (!contract) return;
+      const amountToSend = ethers.utils.parseUnits(amount, 18);
+      console.log("amount to sender", amountToSend);
+      setAmount(amountToSend);
+      const transferData = await contract.transfer(receiver, amountToSend);
+      console.log("Transfer", transferData);
+      await contract.wait();
+
+      const updatedBalance = await contract.getBalance(walletAddress);
+      setBalance(ethers.utils.formatUnits(updatedBalance, 18));
+    } catch (error) {
+      console.log("error transfer", error);
     }
   };
   return (
@@ -91,19 +116,12 @@ const WalletComponent = () => {
       <div className="flex justify-between items-center mb-2">
         <h2 className="text-2xl font-bold">{tokenName} Token Dashboard</h2>
         <div className="flex items-center gap-4">
-          {isConnected && <span className="text-sm">{walletAddress}</span>}
-          <button onClick={connectWallet}>
-            {isConnected
-              ? "Connecting .."
-              : isConnected
-              ? "Disconnect"
-              : "Connect wallet"}
-          </button>
-        </div>
-      </div>
-      {!isConnected ? (
-        <>
-          <div className="flex flex-col justify-between">
+          {isConnected ? (
+            <>
+              <span className="text-sm">{walletAddress}</span>
+              <button onClick={disconnect}>Disconnect</button>
+            </>
+          ) : (
             <button onClick={connectWallet}>
               {isConnected
                 ? "Connecting .."
@@ -111,6 +129,13 @@ const WalletComponent = () => {
                 ? "Disconnect"
                 : "Connect wallet"}
             </button>
+          )}
+        </div>
+      </div>
+      {!isConnected ? (
+        <>
+          <div className="flex flex-col justify-between">
+            <button onClick={disconnect}>Disconnect</button>
             <h2 className="text-2xl font-bold">Connect Your Wallet</h2>
             <p className="">
               Please Connect to interact with the Token smart Contract
@@ -122,11 +147,35 @@ const WalletComponent = () => {
           <h1>Token Information</h1>
           <div className="flex justify-between items-center">
             <p>Your Balance</p>
-            <p>{parseFloat(balance).toFixed(3)} {tokenSymbol}</p>
+            <p>
+              {parseFloat(balance).toFixed(3)} {tokenSymbol}
+            </p>
           </div>
           <div className="flex justify-between items-center">
-          <p>Total Supply</p>
-          <p>{parseFloat(totalSupply).toFixed(3)} {tokenSymbol}</p>
+            <p>Total Supply</p>
+            <p>
+              {parseFloat(totalSupply).toFixed(3)} {tokenSymbol}
+            </p>
+          </div>
+          <div>
+            <input
+              type="text"
+              name="receiver"
+              // value={receiver}
+              onChange={() => setReceiver(receiver)}
+              className="border border-amber-100 p-2 mr-4"
+            />
+            <input
+              type="text"
+              name="amount"
+              // value={receiver}
+              onChange={() => setAmount(amount)}
+              className="border border-amber-100 p-2 mr-4"
+            />
+
+            <button onClick={handleTransfer} className="bg-black text-white">
+              Transfer
+            </button>
           </div>
         </div>
       )}
