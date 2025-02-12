ec6f10d0302ff07d415e29778e496785886e4b44
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2025-02-12 17:50:33 +0300

    feat: add error handling and new functions for token management

--- src/NunoToken.sol
+++ src/NunoToken.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.24;
 
 contract NunoToken {
 
+  // Error
+  error Not_enough_balaance();
+  error Invalid_Input();
+
   // Name and Symbol
   string public name = "NUNOTOKEN";
   string public symbol = "NUNO";
@@ -16,7 +20,7 @@ contract NunoToken {
   mapping (address => mapping(address => uint256)) public allowance;
 
   constructor (uint256 _totalSupply) {
-    totalSupply = _totalSupply*10**decimal;
+    totalSupply = _totalSupply * 10 ** decimal;
     balanceOf[msg.sender] = totalSupply;
   }
 
@@ -52,9 +56,49 @@ contract NunoToken {
 
 
   // burn
+  function burn(uint256 _value) private returns(bool successs) {
+    if (balanceOf[msg.sender] < _value) {
+      revert Not_enough_balaance();
+    }
 
+    balanceOf[msg.sender] -= _value;
+    totalSupply -= _value;
+    return true;
+  }
 
+  // Airdrop
+  function airdrop(address[] memory _recipients, uint256[] memory _values) external returns (bool success) {
+    if (_recipients.length != _values.length) {
+      revert Invalid_Input();
+    }
 
+    for (uint256 i = 0; i < _recipients.length; i++) {
+      addTokens(_recipients[i], 10);
+    }
+    return true;
+  }
 
+  function addTokens(address _to, uint256 _value) internal returns(bool success) {
+    if (balanceOf[msg.sender] < _value) {
+      revert Not_enough_balaance();
+    }
+    balanceOf[_to] += _value;
+    return true;
+  }
 
+  function getName() external view returns(string memory) {
+    return name;
+  }
+
+  function getSymbol() external view returns(string memory) {
+    return symbol;
+  }
+
+  function getTotalSupply() external view returns (uint256) {
+    return totalSupply;
+  }
+
+  function getBalance(address _owner) external view returns(uint256) {
+    return balanceOf[_owner];
+  }
 }
